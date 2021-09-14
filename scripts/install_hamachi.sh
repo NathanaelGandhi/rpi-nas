dfd9172322ff11abcf9b05be321e12259d7ce51d
Nathanael Gandhi <nat11public@gmail.com>
2021-09-15 00:16:37 +0100

    Add install logmein-hamachi-2.1.0.203-armel.tgz script

---
+++ scripts/install_hamachi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wget https://www.vpn.net/installers/logmein-hamachi-2.1.0.203-armel.tgz;
+
+tar -xvzf logmein-hamachi-*;
+
+rm -r logmein-hamachi-*.tgz;
+
+cd logmein-hamachi-*/;
+
+sudo ./install.sh;
+
+
+
+
+
