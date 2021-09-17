5c4d219cd63f46c946b43ee1ea955bb470fa79d6
Nathanael Gandhi <nat11public@gmail.com>
2021-09-17 06:06:19 +0100

    Add comments and terminal instructions

--- scripts/add_to_fstab.sh
+++ scripts/add_to_fstab.sh
@@ -9,6 +9,7 @@ sudo echo "##Format Parity Disks Using: mkfs.ext4 -m 0 -T largefile4 DEVICE" >> 
 sudo echo "##Format Data Disks Using: mkfs.xfs DEVICE" >> /etc/fstab;
 sudo echo "" >> /etc/fstab;
 sudo echo "## Vault1 (8-disk)" >> /etc/fstab;
+# You need to find and provide each drives UUID below. This allows fstab to mount the same drive at the same location everytime, regardless of the physical drive bay used.
 sudo echo "#UUID=  /mnt/parity0   ext4    defaults        0       0       #Pos1: ?TB BRAND MODEL" >> /etc/fstab;
 sudo echo "#UUID=  /mnt/parity1   ext4    defaults        0       0       #Pos2:" >> /etc/fstab;
 sudo echo "UUID=   /mnt/disk0   xfs     defaults        0       0       #Pos3:" >> /etc/fstab;
@@ -29,4 +30,10 @@ sudo echo "#UUID=  /mnt/disk10   xfs     defaults        0       0       #PosX:"
 sudo echo "#UUID=  /mnt/disk11   xfs     defaults        0       0       #PosX:" >> /etc/fstab;
 sudo echo "" >> /etc/fstab;
 sudo echo "### MERGERFS" >> /etc/fstab;
+# This tells Mergerfs to combine all drives at /mnt/disk(1,2,3...etc) and to keep 50gb per drive free (useful for snapraid to function properly)
 sudo echo "/mnt/disk* /mnt/vault fuse.mergerfs direct_io,defaults,allow_other,minfreespace=50G,fsname=mergerfs 0 0" >> /etc/fstab;
+
+echo "[NG] fstab has been updated with the required template.";
+# I am not sure how / if it is safe to automate filling the UUIDs as this is specific to your unique setup.
+echo "[NG] You are required to fill in the UUID of your connected drives manually at /etc/fstab";
+
