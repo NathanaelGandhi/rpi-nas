153e97cb6567fbc4211d0597b7052b66cff1965f
Nathanael Gandhi <nat11public@gmail.com>
2021-09-17 05:39:54 +0100

    Add Author, Last date modified & Platforms tested

--- scripts/enable_temp_polling.sh
+++ scripts/enable_temp_polling.sh
@@ -1,3 +1,7 @@
+## https://github.com/NathanaelGandhi
+## Last Modified: 17 September 2021
+## Tested on: Raspberry Pi 4b - Raspberry Pi OS Lite (May 7th 2021)
+
 #!/bin/bash
 
 echo "[NG] Copying temperature polling script to home directory (~/)";
