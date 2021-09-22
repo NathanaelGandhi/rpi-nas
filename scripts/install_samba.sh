1f6a142a01315df64eeb6fa1dba3b2881aac05fd
Nathanael Gandhi <nat11public@gmail.com>
2021-09-22 19:07:32 +1000

    Add apt update & apt install samaba

--- scripts/install_samba.sh
+++ scripts/install_samba.sh
@@ -1,3 +1,9 @@
 ## https://github.com/NathanaelGandhi
 ## Last Modified: 22 September 2021
 ## Tested on: Raspberry Pi 4b - Raspberry Pi OS Lite (May 7th 2021)
+
+#!/bin/bash
+sudo apt update -y &&
+sudo apt install -y samba samba-common-bin;
+
+
